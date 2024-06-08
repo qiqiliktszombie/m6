ea030626bc0aa7e36e39b90c16f29bdd04acc7b9
qiqiliktszombie <BloomFairy39@gmail.com>
2024-06-08 17:56:28 +0300

    finish

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,18 @@
+
 <template>
-    <section>fantasy</section>
+<section>
+<h1>Range of games</h1>
+</section>
 </template>
+
+<style scoped>
+h1 {
+    margin-top: 60px;
+    font-size: 40px;
+    padding: 10px;
+    background: grey;
+    color: white;
+    border-radius: 20px;
+    text-align: center;
+}
+</style>
--- src/views/Royale.vue
+++ src/views/Royale.vue
@@ -1,3 +1,24 @@
+<script setup>
+import Card from "../components/Card.vue";
+import {GamesCategory} from "../api.js";
+import { onBeforeMount, ref } from "vue";
+let games = ref();
+onBeforeMount(async () => {
+  games.value = await GamesCategory("battle-royale");
+});
+
+</script>
 <template>
-    <section>fantasy</section>
+  <section>
+    <Card v-for="i in games" :key="i" :game="i" />
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  padding: 20px;
+}
+</style>
